17d37bc4a78ab52b1998ca354ded3eaad298ea99
Olivier Laviale <olivier.laviale@gmail.com>
2024-11-02 23:36:20 +0100

    Remove header comments

--- lib/ProxyDumper.php
+++ lib/ProxyDumper.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the olvlvl/symfony-dependency-injection-proxy package.
- *
- * (c) Olivier Laviale <olivier.laviale@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace olvlvl\SymfonyDependencyInjectionProxy;
 
 use InvalidArgumentException;
